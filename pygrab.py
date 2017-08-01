fb23a3bc8f408273f1395b3f2697684e49da770c
Jacob Mansfield <jacob.mansfield@itvet.co.uk>
2017-08-01 11:35:50 +0100

    XLMTV is fussy about element order

--- pygrab.py
+++ pygrab.py
@@ -11,6 +11,38 @@ from lxml import etree
 __version__ = "0.0.1"
 __author__ = "Jacob Mansfield"
 
+ELEMENT_ORDER_WEIGHTS = {
+    # tv element
+    "channel": 1,
+    "programme": 2,
+
+    # Programme element
+    "title": 1,
+    "sub-title": 2,
+    "desc": 3,
+    "credits": 4,
+    "date": 5,
+    "category": 6,
+    "keyword": 7,
+    "language": 8,
+    "orig-language": 9,
+    "length": 10,
+    "icon": 11,
+    "url": 12,
+    "country": 13,
+    "episode-num": 14,
+    "video": 15,
+    "audio": 16,
+    "previously-shown": 17,
+    "premiere": 18,
+    "last-chance": 19,
+    "new": 20,
+    "subtitles": 21,
+    "rating": 22,
+    "star-rating": 23,
+    "review": 24,
+}
+
 
 def __main__():
     config = configuration.Config()
@@ -22,6 +54,10 @@ def __main__():
         print("\n{0} - {1}".format(datetime.datetime.now(), channel.name))
         config.sites[channel.site].parse(channel, data)
         time.sleep(config.sites[channel.site].channel_delay)
+
+    for parent in data.xpath('//*[./*]'):
+        # XLMTV is fussy about element order, so we'll sort everything here to make sure it's the right way round
+        parent[:] = sorted(parent, key=lambda x: ELEMENT_ORDER_WEIGHTS[x.tag])
 
     data.getroottree().write(config.filename, pretty_print=True)
 
